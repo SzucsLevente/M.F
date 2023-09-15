f3fbb3aa22ca95de98bd3d86cd8bc4b12f136ab9
SzucsLevente <Leventeszucs2005@gmail.com>
2023-09-15 12:29:36 +0200

    drugs

---
+++ learn.py
@@ -0,0 +1,42 @@
+'''print("hello world!")
+#print("szia" + 12) szöveg plusz szám egggyenlő nahh nahh (Habbide....)
+print('-'*10)
+
+print("""szia
+gyíík""")
+print('-'*10)
+
+print("szia",end = ' ' )
+print("gyíík")
+print('-'*10)
+
+print("szia",end = '...' )
+print("gyíík") 
+print('-'*10)
+
+print(1,2,3,4,5,6) 
+print('-'*10) 
+
+print(1,2,3,4,5,6, sep= ", ")'''
+'''print('-'*10)
+
+print("szia\npista") # sortörés
+print('-'*10)
+print("szia\apista") # ???
+print('-'*10)
+print("szia\bpista") #back space
+print('-'*10)
+print("szia\tpista") #tabulátor
+print('-'*10)
+print("szia\fpista") # sortörés és tabulátor
+print('-'*10)
+print("szia\rpista") # sor eleje
+print('-'*10)
+print("szia\vpista") # sortörés és tabulátor'''
+
+x = 12
+y = 23
+print(x,y,x*y)
+
+x = x ** 2
+print(x,y,x*y)
