f468487def805c6a1b57e5085479d9495af02bd5
SzucsLevente <Leventeszucs2005@gmail.com>
2023-09-22 11:29:06 +0200

    valtozok

---
+++ valtozok.py
@@ -0,0 +1,59 @@
+'''egy = 1
+print(egy)
+print(type(egy))
+
+#egy = 2    ez egy fasság
+#print(egy)
+
+szám = 12    #no ékezet ékezet nem szerencsés sose rakj ekezetet!!!
+print(type(szám),szám)
+
+szam = 13
+print(type(szam),szam)
+
+Szam = 303      #kis-nagy betű érzékeny
+print(type(Szam),Szam)
+
+szam = szam + egy
+print(szam)
+
+szam /= 10
+print(szam)
+
+szam = 17
+
+szam //= 3 #egész osztás
+print(szam)
+
+szam = 17
+
+szam %= 3 #osztási maradék
+print(szam)
+
+szam = "Alma"*5 #gyökérség
+print(type(szam),szam)
+
+
+tp = 1,3
+print(type(tp),tp)'''
+
+tp = (1,3,"a",3)
+print(type(tp),tp)
+
+x= [1,3,"a",3]
+print(type(x),x)
+
+x= {1,3,"a",3}
+print(type(x),x)
+
+x,y = 12,"pipacs"
+print(x)
+print(y)
+
+#x,y,z = 1,2,
+#x,y,z = 1,2,3,4
+x,y,z = 1,2,3
+
+
+
+
