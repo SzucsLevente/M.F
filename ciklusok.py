62461c44272f5242d5598d59af53176408e2032a
SzucsLevente <Leventeszucs2005@gmail.com>
2023-09-29 11:30:20 +0200

    3.óra

--- ciklusok.py
+++ ciklusok.py
@@ -1,1 +1,43 @@
-szamok = [1,2,5,6,9]
+'''szamok = [1,2,5,6,9]
+for szam in szamok:
+    print(szam)
+    x = szam
+    x += 10
+    print(x)'''
+
+'''elemek = {"alma",12,34,"k",[1,2]} #halmazba nem lehet lista'''
+
+'''elemek = ("alma",12,3.4,"k",[1,2]) #tuple
+for x in elemek:
+    print(type(x),x)'''
+
+'''elemek = ["alma",12,3.4,"k",[1,2]] #lista
+for x in elemek:
+    print(type(x),x)'''
+
+'''for i in range(1,11):
+    print(i,i**2,sep="^2 = ")     # i**2 == i*i'''
+
+'''for j in range(2,101,2):
+    print(j,j*j*j,sep="^3 = ")'''
+
+'''for m in range(10,-11,-3):
+    print(m)'''
+
+'''lista = [1,23,44,5,8,12]
+x=0
+for e in lista:
+    print(x,e)
+    x += 1
+
+print(len(lista))
+#print(lista[6])'''
+
+'''lista = [1,23,44,5,8,12]
+for k in range(len(lista)):
+    print(k,lista[k])'''
+
+
+
+
+
